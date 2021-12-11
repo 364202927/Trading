7dfcc7e89ed3a2650d3d7fa7d146219716a079a5
Agzer0 <364202927@qq.com>
2021-12-11 15:07:52 +0800

    初始提交

---
+++ information.py
@@ -0,0 +1,19 @@
+#交易所api
+information = {"binance":{
+					'apiKey':"",
+					'secret':"",
+					"maxLimit":1000,
+					"fmaxLimit":1500},
+
+				"okex3":{
+					'apiKey':"",
+					'secret':"",
+					"maxLimit":300,
+					"fmaxLimit":300,
+					'url':'https://www.okex.com/'},
+					
+				"bybit":{
+					'apiKey':"",
+					'secret':"",
+					"maxLimit":100,
+					"fmaxLimit":100},}
